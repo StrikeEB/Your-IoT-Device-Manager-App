ce60bd1796cc97d968f21b72401f80301d2e1435
StrikeEB <edi.btk@gmail.com>
2023-04-22 18:31:00 +0100

    send data to firebase

--- lib/logdevicepage.dart
+++ lib/logdevicepage.dart
@@ -1,7 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:what3words/what3words.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'customappbar.dart';
 
@@ -19,9 +24,75 @@ class LogDevicePage extends StatefulWidget {
 }
 
 class _LogDevicePageState extends State<LogDevicePage> {
+  final storage = FirebaseStorage.instance;
   final controllerWhat3Words = TextEditingController();
-  final controllerLong = TextEditingController();
-  final controllerLat = TextEditingController();
+  final controllerDeviceID = TextEditingController();
+  final controllerDeviceMake = TextEditingController();
+  final controllerDeviceModel = TextEditingController();
+  String? _currentAddress;
+  Position? _currentPosition;
+
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    final messenger = ScaffoldMessenger.of(context);
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      await Future.delayed(const Duration(seconds: 5));
+      messenger.showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        await Future.delayed(const Duration(seconds: 5));
+        messenger.showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      await Future.delayed(const Duration(seconds: 5));
+      messenger.showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+
+    if (!hasPermission) return;
+    try {
+      _currentPosition = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      setState(() {});
+      await _getAddressFromLatLng(_currentPosition!);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  Future<void> _getAddressFromLatLng(Position position) async {
+    try {
+      final placemarks =
+          await placemarkFromCoordinates(position.latitude, position.longitude);
+      Placemark place = placemarks[0];
+      setState(() {
+        _currentAddress =
+            '${place.street}, ${place.subLocality}, ${place.subAdministrativeArea}, ${place.postalCode}';
+      });
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -29,40 +100,82 @@ class _LogDevicePageState extends State<LogDevicePage> {
         body: ListView(
           padding: EdgeInsets.all(16),
           children: <Widget>[
+            const SizedBox(height: 32),
+            TextField(
+              controller: controllerDeviceID,
+              decoration: decoration('Device number'),
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              controller: controllerDeviceMake,
+              decoration: decoration('Device make'),
+            ),
+            const SizedBox(height: 24),
+            TextField(
+              controller: controllerDeviceModel,
+              decoration: decoration('Device model'),
+            ),
+            const SizedBox(height: 24),
+            Text(
+              'Latitude: ${_currentPosition?.latitude ?? ""}',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 24),
+            Text(
+              'Longitude: ${_currentPosition?.longitude ?? ""}',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 24),
+            Text(
+              'Address: ${_currentAddress ?? ""}',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromRGBO(2, 50, 82, 1),
+                ),
+                onPressed: _getCurrentPosition,
+                child: const Text("Get Current Location")),
+            const SizedBox(height: 32),
             TextField(
               controller: controllerWhat3Words,
               decoration: decoration('What3Words'),
             ),
             const SizedBox(height: 24),
-            TextField(
-              controller: controllerLong,
-              decoration: decoration('Long'),
-              keyboardType: TextInputType.number,
-            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromRGBO(2, 50, 82, 1),
+                ),
+                onPressed: _getCurrentPosition,
+                child: const Text("Get What3Words")),
             const SizedBox(height: 24),
-            TextField(
-              controller: controllerLat,
-              decoration: decoration('Lat'),
-              keyboardType: TextInputType.number,
-            ),
-            const SizedBox(height: 24),
-            const SizedBox(height: 32),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: Color.fromRGBO(2, 50, 82, 1),
               ),
-              child: Text('Submit'),
               onPressed: () {
                 final device = Device(
                   what3Words: controllerWhat3Words.text,
-                  long: int.parse(controllerLong.text),
-                  lat: int.parse(controllerLat.text),
+                  deviceid: controllerDeviceID.text,
+                  devicemake: controllerDeviceMake.text,
+                  devicemodel: controllerDeviceModel.text,
                 );
 
-                createDevice(device);
-
                 Navigator.pop(context);
+
+                Map<String, String> dataToSave = {
+                  'id': controllerDeviceID.text,
+                  'What3Words': controllerWhat3Words.text,
+                  'Device number': controllerDeviceID.text,
+                  'Device make': controllerDeviceMake.text,
+                  'Device model': controllerDeviceModel.text,
+                };
+
+//add data to the database
+                FirebaseFirestore.instance.collection("Device").add(dataToSave);
               },
+              child: Text('Submit'),
             )
           ],
         ),
@@ -72,34 +185,28 @@ class _LogDevicePageState extends State<LogDevicePage> {
         labelText: label,
         border: OutlineInputBorder(),
       );
-
-  Future createDevice(Device device) async {
-    final docDevice =
-        FirebaseFirestore.instance.collection('Device').doc('my-id');
-    device.id = docDevice.id;
-
-    final json = device.toJson();
-    await docDevice.set(json);
-  }
 }
 
 class Device {
   String id;
   final String what3Words;
-  final int long;
-  final int lat;
+  final String deviceid;
+  final String devicemake;
+  final String devicemodel;
 
   Device({
     this.id = '',
     required this.what3Words,
-    required this.long,
-    required this.lat,
+    required this.deviceid,
+    required this.devicemake,
+    required this.devicemodel,
   });
 
   Map<String, dynamic> toJson() => {
         'id': id,
         'What3Words': what3Words,
-        'Long': long,
-        'Lat': lat,
+        'Device number': deviceid,
+        'Device make': devicemake,
+        'Device model': devicemodel,
       };
 }
