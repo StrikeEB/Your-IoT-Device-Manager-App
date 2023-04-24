7e7ae92341e8440b028d8fe8a4a113d86434df4c
StrikeEB <edi.btk@gmail.com>
2023-04-24 22:18:57 +0100

    w3w in logdevicepage when click on get w3w button

--- lib/logdevicepage.dart
+++ lib/logdevicepage.dart
@@ -162,7 +162,47 @@ class _LogDevicePageState extends State<LogDevicePage> {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Color.fromRGBO(2, 50, 82, 1),
                 ),
-                onPressed: _getCurrentPosition,
+                onPressed: () {
+                  void main() async {
+                    // For all requests a what3words API key is needed
+                    var api = What3WordsV3('QQVQ4KZ6');
+
+                    // Get the user's location
+                    Position position = await Geolocator.getCurrentPosition(
+                        desiredAccuracy: LocationAccuracy.high);
+
+                    // Convert the coordinates to a 3 word address
+                    var words = await api
+                        .convertTo3wa(
+                            Coordinates(position.longitude, position.latitude))
+                        .language('en')
+                        .execute();
+
+                    if (words.isSuccessful()) {
+                      var what3words = words.data()?.toJson();
+                      print('Words: ${words.data()?.toJson()}');
+                    } else {
+                      var error = words.error();
+
+                      if (error == What3WordsError.BAD_COORDINATES) {
+                        // The coordinates provided were bad
+                        print('BadCoordinates: ${error!.message}');
+                      } else if (error == What3WordsError.BAD_LANGUAGE) {
+                        // The language provided was bad
+                        print('BadLanguage: ${error!.message}');
+                      } else if (error ==
+                          What3WordsError.INTERNAL_SERVER_ERROR) {
+                        // Server Error
+                        print('InternalServerError: ${error!.message}');
+                      } else if (error == What3WordsError.NETWORK_ERROR) {
+                        // Network Error
+                        print('NetworkError: ${error!.message}');
+                      } else {
+                        print('${error!.code} : ${error.message}');
+                      }
+                    }
+                  }
+                },
                 child: const Text("Get What3Words")),
             const SizedBox(height: 24),
             ElevatedButton(
