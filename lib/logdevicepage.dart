6b0db705834bbdb66aad2d3dbfe6ed952371803d
StrikeEB <edi.btk@gmail.com>
2023-04-24 21:35:20 +0100

    deleted unused package

--- lib/logdevicepage.dart
+++ lib/logdevicepage.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:what3words/what3words.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
